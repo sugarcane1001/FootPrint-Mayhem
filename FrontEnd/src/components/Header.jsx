60e34a3875a20a86671c7ea4f92dcae3ddae4882
Fezan Ahmed <crescent4prez@gmail.com>
2024-10-11 23:15:57 -0400

    Finished header UI

--- FrontEnd/src/components/Header.jsx
+++ FrontEnd/src/components/Header.jsx
@@ -4,12 +4,12 @@ const Header = () => {
     return (
         <header className="flex items-center justify-around bg-white p-4 shadow-lg">
             <div className="flex items-center">
-                <img src="" alt="App Logo" className="h-10" />
-                <h1 className="text-2xl font-bold ml-2">Footprint Mayhem</h1>
+                <img src="https://i0.wp.com/energysavingpros.com/wp-content/uploads/2017/09/footprint-e1505497060105.jpg?fit=900%2C471&ssl=1" alt="App Logo" className="h-10"/>
+                <h1 className="text-2xl font-bold ml-2 font-mono cursor-default">Footprint Mayhem</h1>
             </div>
-            <div className="flex space-x-4">
-                <button className="bg-green-500 text-white px-4 py-2 rounded hover:bg-green-600 transition">Get Started</button>
-                <button className="bg-transparent text-green-500 border border-green-500 px-4 py-2 rounded hover:bg-green-100 transition">I Already Have an Account</button>
+            <div className='flex items-center justify-between'>
+                <a className="mr-8 text-gray-800 hover:text-green-400 transition delay-30 text-lg font-mono" href=''>Home</a>
+                <a className="text-gray-800 hover:text-green-400 transition delay-30 text-lg font-mono" href=''>About</a>
             </div>
         </header>
     );
